9637b2f51219e31bb19cce0b08f0c22b90f1167f
chen cx <chenchaoxu@taqu.cn>
2019-09-09 17:05:23 +0800

    多语言工具类

---
+++ Example/Pods/Target Support Files/Pods-XHBLanguageUtils_Example/Pods-XHBLanguageUtils_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XHBLanguageUtils_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XHBLanguageUtils_Example
+@end
---
+++ Example/Pods/Target Support Files/XHBLanguageUtils/XHBLanguageUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XHBLanguageUtils : NSObject
+@end
+@implementation PodsDummy_XHBLanguageUtils
+@end
